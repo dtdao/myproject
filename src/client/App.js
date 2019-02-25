bca89058d8608bd3a5579c5bad30942d70d96ab3
kwality <kwalitycontrol@gmail.com>
2019-02-25 20:58:18 +0900

    Updated

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
+// import ReactImage from './react.png';
 
 export default class App extends Component {
   state = { username: null };
@@ -15,8 +15,6 @@ export default class App extends Component {
     const { username } = this.state;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
       </div>
     );
   }
