dbd845bc2efc07562541880702ac6401dd28f2ea
kwality <kwalitycontrol@gmail.com>
2019-03-04 21:54:29 +0900

    mainly worked on caculator app

--- src/client/Calculator/calculator.js
+++ src/client/Calculator/calculator.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './calculator.css'
 
-
 const DisplayMessage = (props) => {
 	return (
 		<div>
@@ -11,33 +10,60 @@ const DisplayMessage = (props) => {
 		)
 }
 
-const GenerateKeys = () => {
+const GenerateKeys = (props) => {
 	let buttons = []
-	for (let i = 0; i < 20; i++ ) {
-		buttons.push(<button key={i+1}>{i+1}</button>)
+	for (let i = 0; i < props.value.length; i++ ) {
+		buttons.push(
+				<button type="button" key={props.value[i]} className="btn btn-primary" onClick={props.click}>{props.value[i]}</button>)
 	}
 	return buttons
-
 }
 
 export default class Calculator extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			display_value:0
-
+			display_value: 0,
+			stored_value: 0,
+			first_row: ['CE','C','DELETE','\u00F7'],
+			second_row: [7, 8, 9, '\u002A'],
+			third_row: [4, 5, 6, '\u002D'],
+			fourth_row: [1, 2, 3, '\u002B'],
+			fif_row: ['\u00B1', 0, '\u002E', '\u003D']
 		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick = (event) =>{
+		let holder = this.state.display_value
+		this.setState({
+			display_value: holder + event.target.innerHTML
+		})
 	}
 
 	render(){
-		const {display_value} = this.state
+		const {display_value, first_row, second_row, third_row, fourth_row, fif_row} = this.state
 		return(
 				<div className="container">
 					<div className="row justify-content-md-center">
 						<div className="well">
 							<div className="col-xs-6 col-xs-offset-3">
 								<DisplayMessage display={display_value} />
-								<GenerateKeys />
+								<div className="btn-group btn-block">
+									<GenerateKeys value={first_row} click={this.handleClick}/>
+								</div>
+								<div className="btn-group btn-block">
+									<GenerateKeys value={second_row} click={this.handleClick}/>									
+								</div>
+								<div className="btn-group btn-block">
+									<GenerateKeys value={third_row} click={this.handleClick}/>
+								</div>
+								<div className="btn-group btn-block">
+									<GenerateKeys value={fourth_row} click={this.handleClick}/>
+								</div>
+								<div className="btn-group btn-block">
+									<GenerateKeys value={fif_row} click={this.handleClick}/>
+								</div>
 							</div>
 						</div>
 					</div>
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const os = require('os');
+const router = express.Router()
+
 
 const app = express();
 
+
+let sendJSONresponse = function(res, status, content) {
+  console.log('hello world')
+  res.status(status);
+  res.json(content);
+};
+
+
 app.use(express.static('dist'));
 
+app.get('*', function(req, res){
+	res.redirect("/")
+})
+
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+
