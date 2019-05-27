1cafc22e77b1174284d7be4bdf1a1817da55828b
Dong Dao <kwalitycontrol@gmail.com>
2019-05-27 21:52:02 +0900

    small update to new search app

--- src/client/news_search/news_search_app.js
+++ src/client/news_search/news_search_app.js
@@ -4,13 +4,20 @@ import React, {Component} from "react";
 export default class NewsSearch extends Component {
 	constructor(props){
 		super(props)
+
 	}
 
+	render(){
+		this.state = {
+
+		}
+	}
 
 	render(){
 		return(
 			<div>
 				<h1>Place holder for the news app!!</h1>
+				<h1>This is a test for git update</h1>
 			</div>
 		)
 	}
